e5da1dab309bc2854776b39bb61f51258a825766
MAWRAN-DA <mrwanmohmed4444@gmail.com>
2025-06-06 03:39:40 +0300

    ثي
    صيشي

---
+++ Marwan.kt
@@ -0,0 +1,9 @@
+package eu.kanade.tachiyomi.extension.ar.marwan
+
+import eu.kanade.tachiyomi.multisrc.anime.FaselHD.FaselHD
+
+class Marwan : FaselHD(
+    "مروان",
+    "https://www.faselhds.care",
+    "ar"
+)
